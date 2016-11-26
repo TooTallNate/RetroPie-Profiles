041449a1c05a7cf20c8d9caef5513e3dc2f18735
Nathan Rajlich <nathan@tootallnate.net>
2016-11-25 16:17:00 -0800

    mostly working inside RetroPie now :D

--- login.sh
+++ login.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env bash
 
-# 1. display a `dialog` to show who is currently logged in and the Login URL
-# 2. run a server in the background that waits for a `PUBLISH login` event
-#    from the Redis auth server
-# 3. 
+# 1a. read in the retroarch.cfg INI file
+# 1b. prompt for the Login Server URL if it's not already set
+#     in the INI file
+# 2. display a dialog showing the current login status
+# 3. run a `curl` command in the background that waits
+#    for a response from the Login Server instance
+# 4a. set up a new user profile if the FB_ID is new
+# 4b. set up symlinks for the savefile and statestate directories
+#     pointing to the logged in user's dirs
 
-source "$HOME/RetroPie-Setup/scriptmodules/helpers.sh"
+user="$SUDO_USER"
+[[ -z "$user" ]] && user=$(id -un)
+
 source "$HOME/RetroPie-Setup/scriptmodules/inifuncs.sh"
 
-joy2keyStart
-iniConfig " = " '"' "$configdir/all/retroarch.cfg"
+CONFIG_FILE="/opt/retropie/configs/all/retroarch.cfg"
+iniConfig " = " '"' "$CONFIG_FILE"
 
 trap finish TERM
 export TOP_PID=$$
 
-# XXX: get real values from `retroarch.cfg` file
+# "save_profiles_directory" is the directory where user profiles will be
+# stored, and symlinks to the current active profile will be kept as well
 iniGet "save_profiles_directory"
 if [[ -z "$ini_value" ]]; then
-  PROFILES_ROOT="$HOME/RetroPie"
+  PROFILES_ROOT="$HOME/RetroPie/save-profiles"
+  iniSet "save_profiles_directory" "$PROFILES_ROOT"
 else
   PROFILES_ROOT="$ini_value"
 fi
 
+# "save_profiles_login_server" is the Login Server to wait for a login
+# event to come from.
+# Set up a new Login Server instance for each RetroPie setup!
 iniGet "save_profiles_login_server"
 if [[ -z "$ini_value" ]]; then
   TMP_OUTPUT=$(mktemp)
@@ -55,10 +67,10 @@ function show_status_dialog() {
   dialog \
    --colors \
    --yes-label "Cancel" \
-   --no-label "Logout" \
    --ok-label "Cancel" \
+   --no-label "Logout" \
    --title "RetroPie Profiles" \
-   --$BOX_TYPE "Currently logged in as:\n\n    \Zb$CURRENT_NAME\ZB\n\nVisit the following URL on your mobile device to log in:\n\n    \Z4\Zu$LOGIN_SERVER_URL\Z0\ZU\n\nProfiles Dir: $PROFILES_ROOT" \
+   --$BOX_TYPE "Currently logged in as:\n\n    \Zb$CURRENT_NAME\ZB\n\nVisit the following URL on your mobile device to log in:\n\n    \Z4\Zu$LOGIN_SERVER_URL\Z0\ZU\n\nProfiles dir: $PROFILES_ROOT\nConfig file: $CONFIG_FILE" \
    0 0
   rc=$?
   if [[ $rc != 0 ]]; then
@@ -86,10 +98,15 @@ function curl_login() {
     USER_SAVE_STATES="$PROFILES_ROOT/$FB_ID/save-states"
 
     mkdir -p "$USER_SAVE_FILES" "$USER_SAVE_STATES"
+
     rm -rf "$CURRENT_SAVE_FILES"
     ln -s "$USER_SAVE_FILES" "$CURRENT_SAVE_FILES"
     rm -rf "$CURRENT_SAVE_STATES"
     ln -s "$USER_SAVE_STATES" "$CURRENT_SAVE_STATES"
+
+    # not a huge deal if this fails, but we'll try anyways
+    # (i.e. the root is on a NFS drive that doesn't allow permission changes)
+    chown -R $user:$user "$PROFILES_ROOT" 2>/dev/null
 
     iniSet "save_profiles_current_id" "$FB_ID"
     iniSet "save_profiles_current_name" "$FB_NAME"
@@ -107,6 +124,8 @@ function logout_current() {
   rm -rf "$CURRENT_SAVE_FILES" "$CURRENT_SAVE_STATES"
   iniUnset "save_profiles_current_id"
   iniUnset "save_profiles_current_name"
+  iniUnset "savefile_directory"
+  iniUnset "savestate_directory"
   dialog \
    --colors \
    --ok-label "Close" \
@@ -117,7 +136,6 @@ function logout_current() {
 }
 
 function finish() {
-  joy2keyStop
   kill $DIALOG_PID
   kill $CURL_PID
   pkill "$CURL_COMMAND"
