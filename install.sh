30789bbb91b6df425ab4200e308bcb9619719ca1
Nathan Rajlich <nathan@tootallnate.net>
2016-11-26 10:20:38 -0800

    install a PNG logo
    Hat tip 🎩 to EmojiOne for the disk image!
    Will have to give credit in the README for sure.

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 # install script that does the following:
 # - put `login.sh` in `$HOME/RetroPie/retropiemenu/` directory
+# - put `logo.png` in `$HOME/RetroPie/retropiemenu/logos` directory
 # - create a gamelist.xml entry for `login.sh`
 
 
 echo -n "Putting \"login.sh\" in \"$HOME/RetroPie/retropiemenu/\"..."
 cp login.sh "$HOME/RetroPie/retropiemenu/login.sh" || {
   echo -e "\nUnable to put \"login.sh\" in \"$HOME/RetroPie/retropiemenu/\". Aborting."
+  exit 1
+}
+echo " OK!"
+
+
+echo -n "Putting \"logo.png\" in \"$HOME/RetroPie/retropiemenu/logos\"..."
+cp logo.png "$HOME/RetroPie/retropiemenu/logos/save-profiles.png" || {
+  echo -e "\nUnable to put \"logo.png\" in \"$HOME/RetroPie/retropiemenu/logos\". Aborting."
   exit 1
 }
 echo " OK!"
@@ -28,8 +37,8 @@ gamelist_info='\
   <game>\
     <path>.\/login.sh<\/path>\
     <name>Save Profiles<\/name>\
-    <desc>Select which user is logged in for save files and save states.<\/desc>\
-    <image><\/image>\
+    <desc>Select which profile to use for save files and save states.<\/desc>\
+    <image>.\/logos\/save-profiles.png<\/image>\
   <\/game>'
 
 sudo sed -i.bak "/<\/gameList>/ s/.*/${gamelist_info}\n&/" "$gamelistxml" || {
